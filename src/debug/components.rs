5567573f0d31b53b7cbc48d1843cbe954a7bfc61
spoorn <spookump@gmail.com>
2023-04-16 13:48:37 -0700

    Add 3d camera control - pan, zoom, orbit

--- src/debug/components.rs
+++ src/debug/components.rs
@@ -1,4 +1,15 @@
-use bevy::prelude::Component;
+use bevy::prelude::{Component, Vec3};
 
 #[derive(Component)]
 pub struct MouseCoordinateText;
+
+/// Tags an entity as capable of panning and orbiting.
+/// 
+/// From https://bevy-cheatbook.github.io/cookbook/pan-orbit-camera.html
+#[derive(Default, Component)]
+pub struct PanOrbitCamera {
+    /// The "focus point" to orbit around. It is automatically updated when panning the camera
+    pub focus: Vec3,
+    pub radius: f32,
+    pub upside_down: bool,
+}
--- src/debug/server.rs
+++ src/debug/server.rs
@@ -1,57 +1,140 @@
-use bevy::input::mouse::{MouseMotion, MouseScrollUnit, MouseWheel};
-use bevy::prelude::{App, Camera2dBundle, Commands, EventReader, Input, MouseButton, Plugin, Query, Res, Transform, With, Without};
-use bevy_render::prelude::{Camera, OrthographicProjection};
-use crate::debug::{cursor_pos_system, init_cursor_pos_system};
+/// Largely taken from https://bevy-cheatbook.github.io/cookbook/pan-orbit-camera.html.
+
+use bevy::input::mouse::{MouseMotion, MouseWheel};
+use bevy::pbr::DirectionalLightShadowMap;
+use bevy::prelude::{AmbientLight, App, AssetServer, Camera3dBundle, Commands, default, EventReader, info, Input, Mat3, MouseButton, Plugin, Quat, Query, Res, SceneBundle, Transform, Vec2, Vec3, Window, With, Without};
+use bevy::window::PrimaryWindow;
+use bevy_render::prelude::{Camera, Color, Projection};
+
+use crate::debug::components::PanOrbitCamera;
 use crate::player::components::ServerPlayer;
 
-const ZOOM_SPEED: f32 = 0.05;
-const SCAN_SPEED: f32 = 0.5;
+const ZOOM_SPEED: f32 = 0.1;
+const PAN_SPEED: f32 = 1.0;
+const ORBIT_SPEED: f32 = 2.0;
 
 pub struct DebugServerPlugin;
 impl Plugin for DebugServerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup_camera)
-            .add_startup_system(init_cursor_pos_system)
-            .add_system(cursor_pos_system)
-            .add_system(zooming)
-            .add_system(scanning);
+        app.insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 1.0 / 5.0f32,
+        })
+            .insert_resource(DirectionalLightShadowMap { size: 4096 })
+            .add_startup_system(setup_camera)
+            // .add_startup_system(init_cursor_pos_system)
+            // .add_system(cursor_pos_system)
+            .add_system(zoom)
+            .add_system(pan)
+            .add_system(orbit);
     }
 }
 
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
+fn setup_camera(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let camera_translation = Vec3::new(0.7, 0.7, 1.0);
+    let focus = Vec3::new(0.0, 0.3, 0.0);
+    commands.spawn((
+       Camera3dBundle {
+            transform: Transform::from_translation(camera_translation)
+                .looking_at(focus, Vec3::Y),
+            ..default()
+        },
+        PanOrbitCamera {
+            focus,
+            radius: (camera_translation - focus).length(),
+            ..default()
+        }
+    ));
+    
+    // Test
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("models/FlightHelmet/FlightHelmet.gltf#Scene0"),
+        ..default()
+    });
 }
 
-fn zooming(mut camera_query: Query<&mut OrthographicProjection, (With<Camera>, Without<ServerPlayer>)>, mut scroll_evr: EventReader<MouseWheel>) {
-    let mut orthographic_projection = camera_query.single_mut();
-    // zooming
-    for ev in scroll_evr.iter() {
-        match ev.unit {
-            MouseScrollUnit::Line => {
-                orthographic_projection.scale -= ZOOM_SPEED * ev.y;
-                if orthographic_projection.scale < 0.0 {
-                    orthographic_projection.scale = 0.0;
-                }
+fn zoom(mut camera_query: Query<(&mut PanOrbitCamera, &mut Transform), (With<Camera>, Without<ServerPlayer>)>, mut scroll_evr: EventReader<MouseWheel>) {
+    let (mut pan_orbit, mut transform) = camera_query.single_mut();
+    
+    let scroll: f32 = scroll_evr.iter().map(|ev| ev.y).sum::<f32>() * ZOOM_SPEED;
+
+    if scroll.abs() > 0.0 {
+        pan_orbit.radius -= scroll;
+        pan_orbit.radius = f32::max(pan_orbit.radius, 0.05);
+        update_camera_transform(&mut transform, &pan_orbit);
+    }
+    
+    scroll_evr.clear();
+}
+
+fn pan(windows: Query<&Window, With<PrimaryWindow>>, mut camera_query: Query<(&mut PanOrbitCamera, &mut Transform, &Projection), (With<Camera>, Without<ServerPlayer>)>, buttons: Res<Input<MouseButton>>, mut motion_evr: EventReader<MouseMotion>) {
+    if buttons.pressed(MouseButton::Middle) {
+        let mut pan = motion_evr.iter().map(|ev| ev.delta).sum::<Vec2>() * PAN_SPEED;
+        
+        if pan.length_squared() > 0.0 {
+            let (mut pan_orbit, mut transform, projection) = camera_query.single_mut();
+            
+            // make panning distance independent of resolution and FOV
+            let window = get_primary_window_size(&windows);
+            if let Projection::Perspective(projection) = projection {
+                pan *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
             }
-            MouseScrollUnit::Pixel => {
-                orthographic_projection.scale -= ZOOM_SPEED * ev.y;
-                if orthographic_projection.scale < 0.0 {
-                    orthographic_projection.scale = 0.0;
-                }
-            }
+            
+            // translate by local axes
+            let right = transform.rotation * Vec3::X * -pan.x;
+            let up = transform.rotation * Vec3::Y * pan.y;
+            // make panning proportional to distance away from focus point
+            let translation = (right + up) * pan_orbit.radius;
+            pan_orbit.focus += translation;
+
+            update_camera_transform(&mut transform, &pan_orbit);
         }
     }
 }
 
-fn scanning(mut camera_query: Query<&mut Transform, (With<Camera>, Without<ServerPlayer>)>, buttons: Res<Input<MouseButton>>, mut motion_evr: EventReader<MouseMotion>) {
-    if buttons.pressed(MouseButton::Left) {
-        // Left Button is being held down
-        let mut camera_transform = camera_query.single_mut();
+// https://math.stackexchange.com/questions/360286/what-does-multiplication-of-two-quaternions-give
+fn orbit(windows: Query<&Window, With<PrimaryWindow>>, mut camera_query: Query<(&PanOrbitCamera, &mut Transform, &Projection), (With<Camera>, Without<ServerPlayer>)>, buttons: Res<Input<MouseButton>>, mut motion_evr: EventReader<MouseMotion>) {
+    if buttons.pressed(MouseButton::Right) {
+        let mut rotation_move = motion_evr.iter().map(|ev| ev.delta).sum::<Vec2>() * ORBIT_SPEED;
+        
+        if rotation_move.length_squared() > 0.0 {
+            let (pan_orbit, mut transform, projection) = camera_query.single_mut();
+            
+            let window = get_primary_window_size(&windows);
+            // Make rotation speed independent of resolution and fov
+            if let Projection::Perspective(projection) = projection {
+                rotation_move *= Vec2::new(projection.fov * projection.aspect_ratio, projection.fov) / window;
+            }
+            
+            // I think multiplying by PI and 2.0 was arbitrary from the original example?
+            let delta_x = {
+                //let delta = rotation_move.x / window.x * std::f32::consts::PI * 2.0;
+                let delta = rotation_move.x;
+                if pan_orbit.upside_down { -delta } else { delta }
+            };
 
-        for ev in motion_evr.iter() {
-            //println!("Mouse moved: X: {} px, Y: {} px", ev.delta.x, ev.delta.y);
-            camera_transform.translation.x -= ev.delta.x * SCAN_SPEED;
-            camera_transform.translation.y += ev.delta.y * SCAN_SPEED;
+            let delta_y = rotation_move.y; //rotation_move.y / window.y * std::f32::consts::PI;
+            // negative to flip direction
+            let yaw = Quat::from_rotation_y(-delta_x);
+            let pitch = Quat::from_rotation_x(-delta_y);
+            // Not sure why this ordering is correct.  Need to better understand quaternion multiplication properties
+            transform.rotation = yaw * transform.rotation; // rotate around global y axis
+            transform.rotation = transform.rotation * pitch; // rotate around local x axis
+
+            update_camera_transform(&mut transform, pan_orbit);
         }
     }
+}
+
+#[inline]
+fn get_primary_window_size(windows: &Query<&Window, With<PrimaryWindow>>) -> Vec2 {
+    let window = windows.get_single().unwrap();
+    let window = Vec2::new(window.width() as f32, window.height() as f32);
+    window
+}
+
+#[inline]
+fn update_camera_transform(transform: &mut Transform, pan_orbit: &PanOrbitCamera) {
+    let rot_matrix = Mat3::from_quat(transform.rotation);
+    transform.translation = pan_orbit.focus + rot_matrix.mul_vec3(Vec3::new(0.0, 0.0, pan_orbit.radius));
 }
