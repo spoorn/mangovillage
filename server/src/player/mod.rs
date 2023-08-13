4e6005bef96e3ec5a3cdfc20f4a2e5342ea9e4bf
spoorn <spookump@gmail.com>
2023-08-13 04:10:42 -0700

    Attach KinematicCharacterController to players, and ground them constantly
    Issue: https://github.com/spoorn/mangovillage/issues/6

--- server/src/player/mod.rs
+++ server/src/player/mod.rs
@@ -15,7 +15,13 @@ pub mod component;
 pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, broadcast_players.run_if(in_state(ServerState::Running)));
+        app.add_systems(Update, (players_move, broadcast_players).run_if(in_state(ServerState::Running)));
+    }
+}
+
+fn players_move(mut controllers: Query<&mut KinematicCharacterController>) {
+    for mut controller in controllers.iter_mut() {
+        controller.translation = Some(Vec3::new(0.0, 0.0, -1.0));
     }
 }
 
@@ -40,14 +46,25 @@ pub fn spawn_player(commands: &mut Commands, addr: String, id: u32, asset_server
     let mut transform = Transform::from_xyz(-10.0, 0.0, 50.0).with_scale(Vec3::splat(0.05));
     transform.rotate_x(0.0);
     let player_model = PLAYER_MODEL_HANDLE_IDS[player_data.handle_id as usize];
-    commands.spawn(SceneBundle { scene: asset_server.load(player_model), transform, ..default() }).insert(ServerPlayerBundle {
-        server_player: ServerPlayer { addr },
-        player_data,
-        colliders: ColliderBundle {
-            collider: Collider::cuboid(12.0, 12.0, 12.0),
-            rigid_body: RigidBody::Dynamic,
-            rotation_constraints: LockedAxes::ROTATION_LOCKED,
+    commands
+        .spawn(SceneBundle { scene: asset_server.load(player_model), transform, ..default() })
+        .insert(ServerPlayerBundle {
+            server_player: ServerPlayer { addr },
+            player_data,
+            colliders: ColliderBundle {
+                collider: Collider::cuboid(12.0, 12.0, 12.0),
+                rigid_body: RigidBody::KinematicPositionBased,
+                rotation_constraints: LockedAxes::ROTATION_LOCKED,
+                ..default()
+            },
+        })
+        .insert(KinematicCharacterController {
+            translation: Some(Vec3::new(0.0, 0.0, -1.0)),
+            up: Vec3::Z,
+            slide: false,
+            max_slope_climb_angle: 45.0_f32.to_radians(),
+            min_slope_slide_angle: 0.0,
+            apply_impulse_to_dynamic_bodies: false,
             ..default()
-        },
-    });
+        });
 }
